d94ac9d4de18cbea2501307e6e61be631ccf9eae
Marcos <mfos@ic.ufal.br>
2021-06-25 10:12:04 -0300

    Removendo itens do menu lateral e deixando ele fixo

--- src/Componentes/DrawerMenu/index.js
+++ src/Componentes/DrawerMenu/index.js
@@ -23,9 +23,6 @@ function DrawerMenu() {
                   <span className="drawer-text main">atlante</span> */}
                </a>
             </li>
-            <span className="drawer-text drawer-title">
-               Tabelas Principais
-            </span>
             <li className="drawer-item">
                <a href="/companie" className="btn btn-initial drawer-link">
                   <IoBusinessOutline size={23} color="#ffb509" />
@@ -43,7 +40,7 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <FaTasks size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Demandas
@@ -51,7 +48,7 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <BsGraphUp size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Jornada do usuário
@@ -59,7 +56,7 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <RiMedalLine size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Badges do usuário
@@ -67,7 +64,7 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <IoBookmarkOutline size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Conquistas
@@ -75,7 +72,7 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <FaUserGraduate size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Mentores
@@ -83,55 +80,13 @@ function DrawerMenu() {
                </a>
             </li>
             <li className="drawer-item">
-               <a href="/" className="btn btn-initial drawer-link">
+               <a href="#" className="btn btn-initial drawer-link">
                   <RiTeamLine size={23} color="#ffb509" />
                   <span className="drawer-text">
                      Times
                   </span>
                </a>
             </li>
-
-            <ul className="list-unstyled borda-container">
-               <span className="drawer-text drawer-title">
-                  Tabelas Secundárias
-               </span>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <span className="drawer-text item-p">Status das Demandas</span>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">Status dos Usuários</p>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">Resultados das Demandas</p>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">Tipo das Demandas</p>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">  Tipo de Mentor</p>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">Badges</p>
-                  </a>
-               </li>
-               <li className="logo drawer-item">
-                  <a href="#" className="btn btn-initial drawer-link">
-                     <p className="drawer-text item-p">Jornadas</p>
-                  </a>
-               </li>
-            </ul>
-
          </ul>
       </div>
    );
