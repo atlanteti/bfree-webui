2bc27a444d1082a997ef7725cd934c8691844918
wykthor <wykthor.g@wgvale.com>
2021-07-19 14:23:10 -0300

    Estilização da borda da barra de pesquisa

--- src/Componentes/SearchBar/index.js
+++ src/Componentes/SearchBar/index.js
@@ -1,6 +1,7 @@
 import { Row, Form, Button, Col } from 'react-bootstrap'
 import { React, Component } from 'react'
 import ListCompanies from '../ListCompanies'
+import { SearchBarBorder } from '../../styles/styles'
 
 export default class SearchBar extends Component {
   constructor (props) {
@@ -28,30 +29,32 @@ export default class SearchBar extends Component {
     return
   }
   render () {
-    return <Row xs={1} className={'mb-2'} noGutters>
-      <Form onSubmit={this.handleSubmit}>
-         {this.props.children}
-         <Row>
-           <Col>
-             <Form.Group controlId="name">
-                <Form.Label>{this.props.Label}</Form.Label>
-                <Form.Control
-                   type="text"
-                   onChange={this.onChange}
-                   placeholder={this.props.InputPlaceHolder}/>
-             </Form.Group>
-           </Col>
-           <Col>
-             <ListCompanies
-              onChange={this.onChange}
-              controlId={"companyId"}/>
-           </Col>
+    return <SearchBarBorder>
+      <Row xs={1} className={'mb-2'} noGutters>
+        <Form onSubmit={this.handleSubmit}>
+           {this.props.children}
+           <Row>
+             <Col>
+               <Form.Group controlId="name">
+                  <Form.Label>{this.props.Label}</Form.Label>
+                  <Form.Control
+                     type="text"
+                     onChange={this.onChange}
+                     placeholder={this.props.InputPlaceHolder}/>
+               </Form.Group>
+             </Col>
+             <Col>
+               <ListCompanies
+                onChange={this.onChange}
+                controlId={"companyId"}/>
+             </Col>
+           </Row>
+           <Button variant="warning" type="submit">
+              Buscar
+           </Button>
+        </Form>
          </Row>
-         <Button variant="warning" type="submit">
-            Buscar
-         </Button>
-      </Form>
-   </Row>
+    </SearchBarBorder>
   }
 }
 
