ae08456505005aecd8e85b5338abb4f778def296
wykthor <wykthor.g@wgvale.com>
2021-07-26 18:18:44 -0300

    Gera componentes necessários para a execução da página, ajusta rotas

---
+++ src/Componentes/ListUserStatus/index.js
@@ -0,0 +1,63 @@
+import { Form } from 'react-bootstrap';
+import { React, Component } from 'react';
+import { request } from '../../Services/api';
+import PropTypes from 'prop-types'
+export default class ListUserStatusControlled extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        statuses: []
+      };
+    }
+  
+    async getStatus() {
+      const data = await request({
+        method: 'get',
+        endpoint: 'status-users/listar-todos'
+      });
+      this.setState({
+        statuses: data.data
+      });
+    }
+  
+    componentDidMount() {
+      this.getStatus();
+    }
+  
+    onChange(event) {
+      this.props.onChange(event)
+    }
+    
+    render() {
+      return <Form.Group controlId={this.props.controlId} /*"companyId"*/>
+        <Form.Label>Status: </Form.Label>
+        <Form.Control //Form.Select não funciona por razões misteriosas
+  
+          required={this.props.required}
+          as="select"
+          onChange={this.onChange.bind(this)}
+          disabled={this.props.disabled}
+          value={this.props.value}>
+          <>
+            <option value={null} />
+            {this.state.statuses?.map(status => {
+              return (
+                <option
+                  key={status.sus_cod}
+                  value={status.sus_cod}
+                >
+                  {status.sus_name}
+                </option>);
+            })}
+          </>
+        </Form.Control>
+      </Form.Group>;
+    }
+  }
+ListUserStatusControlled.propTypes = 
+{
+  required: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  disabled: PropTypes.string,
+  controlId: PropTypes.string.isRequired
+}
