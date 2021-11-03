d33ae9edabef78a3a3fae00b10dcc8351a0d80e8
wykthor <wykthor.g@wgvale.com>
2021-11-03 11:14:49 -0300

    Adiciona renderização condicional para o formulário de demanda

--- src/Pages/Demandas/Cadastrar/Form.js
+++ src/Pages/Demandas/Cadastrar/Form.js
@@ -162,29 +162,34 @@ export class DemandFormBuilder extends EditCreateForm {
                               />
                            </Col>
                         </Row>
-                           <Row>
-                              <Col className="mt-3" xs={12} sm={6} >
-                                 <DatePicker
-                                    controlId="dem_dtaction"
-                                    placeholderText="dd/mm/aaaa"
-                                    dateFormat="dd/MM/yyyy"
-                                    maxDate={new Date()}
-                                    disabled={!this.context.admin && this.state.primaryData.dem_sdm_cod !== 2} // caso mude a ordem dos status, isso precisa ser refatorado
-                                    selected={this.state.dateAction}
-                                    onChange={(dateSelect) => this.handleDate(dateSelect, "dem_dtaction")}
-                                    customInput={
-                                       <ValidationTextField
-                                          label="Data de Ação"
-                                          type="text"
-                                          InputLabelProps={{
-                                             shrink: true,
-                                          }}
+                        {(this.state.checkStatus === "COMPARECIDO" || this.state.checkStatus === "VENDA") &&
+                           (
+                              <Restricted>
+                                 <Row>
+                                    <Col className="mt-3" xs={12} sm={6}>
+                                       <DatePicker
+                                          controlId="dem_dtaction"
+                                          placeholderText="dd/mm/aaaa"
+                                          dateFormat="dd/MM/yyyy"
+                                          maxDate={new Date()}
+                                          selected={this.state.dateAction}
+                                          onChange={(dateSelect) => this.handleDate(dateSelect, "dem_dtaction")}
+                                          customInput={
+                                             <ValidationTextField
+                                                label="Data de Ação"
+                                                type="text"
+                                                InputLabelProps={{
+                                                   shrink: true,
+                                                }}
+                                             />
+                                          }
                                        />
-                                    }
-                                 />
-                              </Col>
-                           </Row>
-                        {this.props.paramRoute === 'inserir'
+                                    </Col>
+                                 </Row>
+                              </Restricted>
+
+                           )}
+                        {this.paramRoute === 'inserir'
                            ? ''
                            :
                            <Row className="mt-6">
@@ -210,41 +215,42 @@ export class DemandFormBuilder extends EditCreateForm {
                               <BtnBlue variant="dark" type="submit">Salvar</BtnBlue>
                            </Col>
                         </Row>
-                        <Row style={{ marginTop: 25 }}>
-                           <Col>
-                              <Row noGutters>
-                                 <b>Histórico de Alterações de Status:</b>
-                              </Row>
-                              <Row noGutters>
-                                 <MainTable className={"table-borderless"}>
-                                    <TableHeader>
-                                       <TextHeaderCell>
-                                          Status:
-                                       </TextHeaderCell>
-                                       <TextHeaderCell>
-                                          Data de alteração:
-                                       </TextHeaderCell>
-                                    </TableHeader>
-                                    <TableData>
-                                       {this.state.primaryData?.demandStatusHistories.map(
-                                          (entry) => {
-                                             return <TableRow>
-                                                <TextCell data-title="Status:">
-                                                   {entry.statusDemand.sdm_name}
-                                                </TextCell>
-                                                <TextCell data-title="Data de alteração:">
-                                                   <DateField
-                                                      controlId="dem_dtupdate"
-                                                      Label=""
-                                                      date={entry.dsh_dtcreation} />
-                                                </TextCell>
-                                             </TableRow>
-                                          })}
-                                    </TableData>
-                                 </MainTable>
-                              </Row>
-                           </Col>
-                        </Row>
+                        {this.paramRoute !== "inserir" ?
+                           <Row style={{ marginTop: 25 }}>
+                              <Col>
+                                 <Row noGutters>
+                                    <b>Histórico de Alterações de Status:</b>
+                                 </Row>
+                                 <Row noGutters>
+                                    <MainTable className={"table-borderless"}>
+                                       <TableHeader>
+                                          <TextHeaderCell>
+                                             Status:
+                                          </TextHeaderCell>
+                                          <TextHeaderCell>
+                                             Data de alteração:
+                                          </TextHeaderCell>
+                                       </TableHeader>
+                                       <TableData>
+                                          {this.state.primaryData?.demandStatusHistories.map(
+                                             (entry) => {
+                                                return <TableRow>
+                                                   <TextCell data-title="Status:">
+                                                      {entry.statusDemand.sdm_name}
+                                                   </TextCell>
+                                                   <TextCell data-title="Data de alteração:">
+                                                      <DateField
+                                                         controlId="dem_dtupdate"
+                                                         Label=""
+                                                         date={entry.dsh_dtcreation} />
+                                                   </TextCell>
+                                                </TableRow>
+                                             })}
+                                       </TableData>
+                                    </MainTable>
+                                 </Row>
+                              </Col>
+                           </Row> : null}
                      </BackGroundForm>
                   </Form>
                )
