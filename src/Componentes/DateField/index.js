4cfc61fad12a879f9c6f7ad35ec03e1edb3d78c0
wykthor <wykthor.g@wgvale.com>
2021-11-05 17:07:14 -0300

    Cria componente e estilização para mostrar data na listagem

--- src/Componentes/DateField/index.js
+++ src/Componentes/DateField/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Form, Col, Row } from 'react-bootstrap'
 import moment from 'moment'
+import { FormGroup, FormLabel } from './styles'
 export function displayDate(date, noHour) {
    const parsedTime = moment(date).format('hh')
    var formatedHour = (parseInt(parsedTime) + 12) % 24
@@ -23,6 +24,17 @@ export function DateField(props) {
             <Form.Group controlId={props.controlId}>
                <Form.Label style={{ color: "#546E7A" }}>{props.Label} <strong>{displayDate(props.date, props.noHour)}</strong></Form.Label>
             </Form.Group>
+         </Col>
+      </Row>
+   </>
+}
+export function DateFieldStatus(props) {
+   return <>
+      <Row>
+         <Col>
+            <FormGroup controlId={props.controlId}>
+               <FormLabel style={{ color: "#546E7A" }}>{props.Label} {displayDate(props.date, props.noHour)}</FormLabel>
+            </FormGroup>
          </Col>
       </Row>
    </>
