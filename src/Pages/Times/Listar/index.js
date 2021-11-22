c480a4db4313605a2d8b046bf8e4b272f623a99d
alandysson <alandyssom@gmail.com>
2021-11-22 11:29:46 -0300

    Refactor(Times): Adicionado a propriedade de Elipse no nome do time para que textos grandes não quebrem a tabela

--- src/Pages/Times/Listar/index.js
+++ src/Pages/Times/Listar/index.js
@@ -86,7 +86,7 @@ export default class ListarTime extends ListarPagina {
 
    createRecord(time) {
       return <TableRow key={time.tea_cod}>
-         <TextCell data-title="Nome">{time.tea_name}</TextCell>
+         <TextCell data-title="Nome" Elipse>{time.tea_name}</TextCell>
          <TextCell data-title="Empresa" className="text">{time.company.cpn_name ? time.company.cpn_name : <NoDataComp/>}</TextCell>
          <TextCell data-title="Status" className="text">
             {time.tea_active ? <ActiveStatusBadge /> : <InactiveStatusBadge />}
