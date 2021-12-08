ebb526f0f097e5baa9a9a05b360935168efe9780
alandysson <alandyssom@gmail.com>
2021-12-08 10:58:36 -0300

    Style(Resultados): Adicionado espaçamento automatico na pagina

--- src/Pages/Resultados/index.js
+++ src/Pages/Resultados/index.js
@@ -6,57 +6,53 @@ import { MyResponsiveBar } from '../../Componentes/Graph';
 import { request } from '../../Services/api';
 import { CustomMenuCol } from '../../styles/CommonStyles';
 
-export function Resultados(){
-    const [headerData, setHeaderData] = useState(null);
-    const [populateNumbers, setPopulateNumbers] = useState(null);
-    const graph = []
-    const obj = {}
+export function Resultados() {
+   const [headerData, setHeaderData] = useState(null);
+   const [populateNumbers, setPopulateNumbers] = useState(null);
+   const graph = []
+   const populateObject = {}
 
-    async function fetchData() {
-        let data = await request({
-            method: 'get',
-            endpoint: 'demands/report-billing',
-            params: {
-                dataInicial: "2021-11-01",
-                dataFinal: "2021-11-29",
-             }
-        }).then((data) => {
-            let bodyData = data.data.splice(1)
-            setHeaderData(data.data[0].splice(1))
-            const numbers = bodyData.map(line => line.slice(1))
-            const numbersTransposed = numbers[0].map((_, colIndex) => numbers.map(row => row[colIndex]))
-            const reducedSum = numbersTransposed.map(
-                line => line.reduce(
-                    (partial_sum, accumulator) => Number(partial_sum) + Number(accumulator), 0))
-            const lastLine = ["Total"].concat(reducedSum)
-            setPopulateNumbers(reducedSum)
-            bodyData = bodyData.concat([lastLine])
-            data.data = bodyData.map(
-                line => line.map(
-                    (lineData, keyIndex) => { return [data.data[0][keyIndex], lineData] }))
-            return data
-        })
-        return data
-    }
+   async function fetchData() {
+      let data = await request({
+         method: 'get',
+         endpoint: 'demands/report-billing',
+      }).then((data) => {
+         let bodyData = data.data.splice(1)
+         setHeaderData(data.data[0].splice(1))
+         const numbers = bodyData.map(line => line.slice(1))
+         const numbersTransposed = numbers[0].map((_, colIndex) => numbers.map(row => row[colIndex]))
+         const reducedSum = numbersTransposed.map(
+            line => line.reduce(
+               (partial_sum, accumulator) => Number(partial_sum) + Number(accumulator), 0))
+         const lastLine = ["Total"].concat(reducedSum)
+         setPopulateNumbers(reducedSum)
+         bodyData = bodyData.concat([lastLine])
+         data.data = bodyData.map(
+            line => line.map(
+               (lineData, keyIndex) => { return [data.data[0][keyIndex], lineData] }))
+         return data
+      })
+      return data
+   }
 
-    if(populateNumbers?.length){
-        obj['month'] = moment(new Date()).format('MM/YY')
-        for(let i = 0; i < headerData?.length; i ++){
-            obj[headerData[i]] = populateNumbers[i]
-        }
-        graph.push(obj)
-    }
+   if (populateNumbers?.length) {
+      populateObject['month'] = moment(new Date()).format('MM/YY')
+      for (let i = 0; i < headerData?.length; i++) {
+         populateObject[headerData[i]] = populateNumbers[i]
+      }
+      graph.push(populateObject)
+   }
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-    
-    return (
-        <Col>
-            <CustomMenuCol lg={2}><CustomMenu /></CustomMenuCol>
-            <Col style={{height: 400}} md={{offset: 2, span: 9}}>
-                    <MyResponsiveBar data={graph} />
-            </Col>
-        </Col>
+   useEffect(() => {
+      fetchData()
+   }, [])
+
+   return (
+      <Col>
+         <CustomMenuCol lg={2}><CustomMenu /></CustomMenuCol>
+         <Col style={{ height: 400 }} md={{ offset: 2, span: 9 }}>
+            <MyResponsiveBar data={graph} />
+         </Col>
+      </Col>
    )
 }
