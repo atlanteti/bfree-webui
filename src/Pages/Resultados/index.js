682cd227aa19701fa0b666f127161cacbcd52ecd
alandysson <alandyssom@gmail.com>
2021-12-08 11:19:21 -0300

    Feat(Resultados): Adicionado um filtro para pesquisar o resultado

--- src/Pages/Resultados/index.js
+++ src/Pages/Resultados/index.js
@@ -1,10 +1,12 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import { CustomMenu } from "../../Componentes/CustomMenu/index";
+import { ValidationTextField } from '../../Componentes/FormFields';
 import { MyResponsiveBar } from '../../Componentes/Graph';
 import { request } from '../../Services/api';
-import { CustomMenuCol } from '../../styles/CommonStyles';
+import DatePicker from "react-datepicker";
+import { CustomMenuCol, SearchBarBorder, BtnBlue, DataSearchTitle } from '../../styles/CommonStyles';
 
 export function Resultados() {
    const [headerData, setHeaderData] = useState(null);
@@ -48,11 +50,65 @@ export function Resultados() {
    }, [])
 
    return (
-      <Col>
+      <>
          <CustomMenuCol lg={2}><CustomMenu /></CustomMenuCol>
-         <Col style={{ height: 400 }} md={{ offset: 2, span: 9 }}>
-            <MyResponsiveBar data={graph} />
+         <Col>
+            <Col md={{ offset: 2, span: 10 }}>
+               <SearchBarBorder>
+                  <Form onSubmit={() => console.log("a")}>
+                     <Row>
+                        <Col xs={12} md={7} sm={7}>
+                           <DataSearchTitle>Pesquisar por período</DataSearchTitle>
+                           <Row>
+                              <Col className="mt-2">
+                                 <DatePicker
+                                    placeholderText="dd/mm/aaaa"
+                                    dateFormat="dd/MM/yyyy"
+                                    // selected={this.state.initialDate}
+                                    onChange={(dateSelect) => this.changeDate(dateSelect, "initialDate")}
+                                    customInput={
+                                       <ValidationTextField
+                                          label="Data Inicial"
+                                          type="text"
+                                          fullWidth
+                                       />
+                                    }
+                                 />
+                              </Col>
+                              <Col className="mt-2">
+                                 <DatePicker
+                                    placeholderText="dd/mm/aaaa"
+                                    dateFormat="dd/MM/yyyy"
+                                    // selected={this.state.finalDate}
+                                    // maxDate={this.state.finalDate}
+                                    onChange={(dateSelect) => this.changeDate(dateSelect, "finalDate")}
+                                    customInput={
+                                       <ValidationTextField
+                                          label="Data Final"
+                                          type="text"
+                                          fullWidth
+                                       />
+                                    }
+                                 />
+                              </Col>
+                           </Row>
+                        </Col>
+                        <Col className="mt-3" xs={12} md={2} sm={5}>
+                           <p style={{ color: 'transparent' }}>.</p>
+                           <Row>
+                              <Col>
+                                 <BtnBlue type="submit" variant="dark">Buscar</BtnBlue>
+                              </Col>
+                           </Row>
+                        </Col>
+                     </Row>
+                  </Form>
+               </SearchBarBorder>
+               <Col style={{ height: 400 }}>
+                  <MyResponsiveBar data={graph} />
+               </Col>
+            </Col>
          </Col>
-      </Col>
+      </>
    )
 }
