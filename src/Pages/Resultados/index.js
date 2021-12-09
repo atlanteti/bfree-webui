c701af1ec881de50d58130f96024744e160559ba
alandysson <alandyssom@gmail.com>
2021-12-09 15:02:05 -0300

    Refactor(Resultados): Refatorada a requisação, agora o back devolve os dados ja todos prontos para o grafico

--- src/Pages/Resultados/index.js
+++ src/Pages/Resultados/index.js
@@ -1,48 +1,32 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import { CustomMenu } from "../../Componentes/CustomMenu/index";
-import { ValidationTextField } from '../../Componentes/FormFields';
 import { MyResponsiveBar } from '../../Componentes/Graph';
 import { request } from '../../Services/api';
-import DatePicker from "react-datepicker";
-import { CustomMenuCol, SearchBarBorder, BtnBlue, DataSearchTitle } from '../../styles/CommonStyles';
+import { CustomMenuCol } from '../../styles/CommonStyles';
 import ResultadoSearchBar from './ResultadoSearchBar';
 
 export function Resultados() {
-   const [headerData, setHeaderData] = useState(null);
-   const [populateNumbers, setPopulateNumbers] = useState(null);
    const [initialDate, setInitialDate] = useState(new Date(moment().subtract(4, 'month').calendar()));
    const [finalDate, setFinalDate] = useState(new Date);
-   const graph = []
-   const populateObject = {}
+   const [graph, setGraph] = useState([])
 
    async function fetchData() {
-      console.log(initialDate, 1)
       let data = await request({
          method: 'get',
-         endpoint: 'demands/report-billing',
+         endpoint: 'demands/list-graphic',
          params: {
-            dataInicial: moment(initialDate).format('yyyy-MM-DD'),
-            dataFinal: moment(finalDate).format('yyyy-MM-DD'),
+            dataInicial: "2021-11-01",
+            dataFinal: "2021-12-30"
+            // dataInicial: moment(initialDate).format('yyyy-MM-DD'),
+            // dataFinal: moment(finalDate).format('yyyy-MM-DD'),
          }
-      }).then((data) => {
-         let bodyData = data.data.splice(1)
-         setHeaderData(data.data[0].splice(1))
-         const numbers = bodyData.map(line => line.slice(1))
-         const numbersTransposed = numbers[0].map((_, colIndex) => numbers.map(row => row[colIndex]))
-         const reducedSum = numbersTransposed.map(
-            line => line.reduce(
-               (partial_sum, accumulator) => Number(partial_sum) + Number(accumulator), 0))
-         const lastLine = ["Total"].concat(reducedSum)
-         setPopulateNumbers(reducedSum)
-         bodyData = bodyData.concat([lastLine])
-         data.data = bodyData.map(
-            line => line.map(
-               (lineData, keyIndex) => { return [data.data[0][keyIndex], lineData] }))
-         return data
       })
-      return data
+      console.log(data.data)
+      if (data.meta.status === 100) {
+         setGraph(data.data['taxaDeSucesso'])
+      }
    }
 
    function changeDate(date, id) {
@@ -56,14 +40,6 @@ export function Resultados() {
    function handleSubmit(event) {
       event.preventDefault()
       fetchData()
-   }
-
-   if (populateNumbers?.length) {
-      populateObject['month'] = moment(new Date()).format('MM/YY')
-      for (let i = 0; i < headerData?.length; i++) {
-         populateObject[headerData[i]] = populateNumbers[i]
-      }
-      graph.push(populateObject)
    }
 
    useEffect(() => {
