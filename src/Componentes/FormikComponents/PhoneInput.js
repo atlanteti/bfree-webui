83e364dda964c154e957f78069df30db496a591e
wykthor <wykthor.g@wgvale.com>
2021-12-13 11:17:01 -0300

    Refactor(Componente): Campo de telefone tem uma forma mais simples de aplicar máscara

--- src/Componentes/FormikComponents/PhoneInput.js
+++ src/Componentes/FormikComponents/PhoneInput.js
@@ -5,17 +5,10 @@ import { ValidationTextField } from '../FormFields';
 
 export function PhoneInput(props) {
    const [field, meta] = useField(props);
-   let mask = "(99) 9999-99999";
-   if (field.value != undefined) {
-      const emptySpaces = field.value.match(/ /g || []);
-      if (emptySpaces !== null && emptySpaces.length === 1 && field.value.length === 15) {
-         mask = "(99) 99999-9999";
-      }
-   }
    return <InputMask
       id={field.name}
-      mask={mask}
-      maskChar=" "
+      mask={field.value.length < 15 ? "(99) 9999-99999" : "(99) 99999-9999"}
+      maskChar=""
       value={field.value}
       onChange={field.onChange}
       onBlur={field.onBlur}
