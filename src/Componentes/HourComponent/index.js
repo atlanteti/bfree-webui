7943c67b8999f2ffd11ae368c699024b0328f4f8
Alandysson Rolim <alandyssom@gmail.com>
2021-12-22 14:47:20 -0300

    Feat(Horario): Componetizado a linha com os dois seletores de horas para que o codigo fique melhor apresentado

--- src/Componentes/HourComponent/index.js
+++ src/Componentes/HourComponent/index.js
@@ -1,23 +1,53 @@
 import { MenuItem } from "@mui/material";
+import { Row, Col } from "react-bootstrap";
 import { ValidationTextField } from "../FormFields";
 
 export function HourComponent(props) {
    function handleChange(e) {
       console.log(e)
    }
+   return (
+      <ValidationTextField
+         select
+         fullWidth
+         onChange={handleChange}
+         name={props.name}
+         label={props.label}
+         InputLabelProps={{
+            shrink: true,
+            required: false
+         }}
+      >
+         <MenuItem value={"08:00"}>08:00</MenuItem>
+         <MenuItem value={"09:00"}>09:00</MenuItem>
+         <MenuItem value={"10:00"}>10:00</MenuItem>
+         <MenuItem value={"11:00"}>11:00</MenuItem>
+         <MenuItem value={"12:00"}>12:00</MenuItem>
+         <MenuItem value={"13:00"}>13:00</MenuItem>
+         <MenuItem value={"14:00"}>14:00</MenuItem>
+         <MenuItem value={"15:00"}>15:00</MenuItem>
+         <MenuItem value={"16:00"}>16:00</MenuItem>
+         <MenuItem value={"17:00"}>17:00</MenuItem>
+         <MenuItem value={"18:00"}>18:00</MenuItem>
+      </ValidationTextField>
+   )
+}
 
-
-   return <ValidationTextField
-      select
-      fullWidth
-      onChange={handleChange}
-      name={props.name}
-      label={props.label}
-      InputLabelProps={{
-         shrink: true,
-         required: false
-      }}
-   >
-      <MenuItem value={"08:00"}>08:00</MenuItem>
-   </ValidationTextField>
+export function SetHour(props) {
+   return <Row style={{ alignItems: 'center', justifyContent: 'center', paddingTop: 20 }}>
+      <p>{props.dayOfWeek}</p>
+      <Col xs={12} sm={4} lg={2}>
+         <HourComponent
+            label="Inicial"
+            name={props.initialId}
+         />
+      </Col>
+      <p>:</p>
+      <Col xs={12} sm={4} lg={2}>
+         <HourComponent
+            label="Final"
+            name={props.finalId}
+         />
+      </Col>
+   </Row>
 }
--- src/Pages/Horarios/index.js
+++ src/Pages/Horarios/index.js
@@ -1,7 +1,7 @@
 import { MainContainer, MainRow, BackGroundForm } from "../../styles/CommonStyles";
 import { CustomMenu } from "../../Componentes/CustomMenu";
 import { Form, Col, Row } from "react-bootstrap";
-import { HourComponent } from "../../Componentes/HourComponent";
+import { HourComponent, SetHour } from "../../Componentes/HourComponent";
 import { Title, SubTitle } from "./styles.js"
 export function Horario() {
    return (
@@ -20,22 +20,16 @@ export function Horario() {
                      <BackGroundForm xs={1} className={'mb-2'} noGutters>
                         <Title>Vamos configurar sua agenda?</Title>
                         <SubTitle>Defina os dias da semana e horários que você pode atender</SubTitle>
-                        <Row style={{ alignItems: 'center', justifyContent: 'center', paddingTop: 20 }}>
-                           <p>SEG</p>
-                           <Col xs={12} sm={4}>
-                              <HourComponent
-                                 label="Inicial"
-                                 name="col_init_hour"
-                              />
-                           </Col>
-                           <p>:</p>
-                           <Col xs={12} sm={4}>
-                              <HourComponent
-                                 label="Final"
-                                 name="col_end_hour"
-                              />
-                           </Col>
-                        </Row>
+                        <SetHour
+                           dayOfWeek="SEG"
+                           initialId="col_init_hour"
+                           finalId="col_end_hour"
+                        />
+                        <SetHour
+                           dayOfWeek="TER"
+                           initialId="col_init_hour"
+                           finalId="col_end_hour"
+                        />
                      </BackGroundForm>
                   </Form>
                </Col>
