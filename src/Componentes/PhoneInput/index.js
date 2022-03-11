7ad7dcc9028923c44cad52434cb1fb61fbc6d861
alandysson <alandyssom@gmail.com>
2022-03-11 10:44:54 -0300

    Refactor(FormatPhone): Adicionado encadeamento opcional

--- src/Componentes/PhoneInput/index.js
+++ src/Componentes/PhoneInput/index.js
@@ -31,5 +31,5 @@ export function PhoneInput(props) {
 }
 
 export function FormatPhone(data) {
-   return data.replaceAll(/[^\d]/g, "").replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3")
+   return data?.replaceAll(/[^\d]/g, "").replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3")
 }
