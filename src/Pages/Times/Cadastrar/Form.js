caa526e1f2432e2337ff8189296ac00881fef081
Alandysson Rolim <alandyssom@gmail.com>
2022-06-01 11:33:36 -0300

    Refactor(Times): Alterado formulário para o do formik

--- src/Pages/Times/Cadastrar/Form.js
+++ src/Pages/Times/Cadastrar/Form.js
@@ -1,104 +1,139 @@
-import React from 'react'
-import { Form, Col, Row, Button } from 'react-bootstrap'
-import { DateField } from '../../../Componentes/DateField'
+import React, { useEffect, useState } from 'react'
+import { Form, Formik } from 'formik';
+import yup from "../../../Services/validations";
+import { request } from '../../../Services/api';
+import { DefaultValidationTextField } from '../../../Componentes/FormikComponents/DefaultValidationTextField';
+import { Col, Row, Button } from 'react-bootstrap'
 import { ButtonRow } from '../../../Componentes/ButtonRow'
-import { EditCreateForm } from '../../../Componentes/EditCreateForm/index'
-import { BooleanField, InputTextField } from '../../../Componentes/FormFields'
-import ListCompanies from '../../../Componentes/ListCompanies'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import { BackGroundForm, BtnBlue, TitleRegister } from '../../../styles/CommonStyles'
 import { IoChevronBackCircleSharp } from "react-icons/io5"
+import { Timestamps } from '../../../Componentes/FormikComponents/Timestamps';
+import { ListCompanies } from '../../../Componentes/FormikComponents/ListCompanies';
+import { MenuItem } from '@mui/material';
 
-export default function TeamForm(props) {
-   return <TeamFormBuilder insertDataEndpoint="teams/cadastrar"
-      requestDataEndpoint="teams/procurar/"
-      editDataEndpoint="teams/alterar/"
-      {...props} />
-}
-
-export class TeamFormBuilder extends EditCreateForm {
-   render() {
-      return (
-         <>
-            {this.state.loading && this.paramRoute !== 'inserir'
-               ?
-               <Row>
-                  <Col md={{ offset: 6 }}><CircularProgress /></Col>
-               </Row>
-               :
-               (
-                  <Form onSubmit={this.handleSubmit} validated={this.state.validated} noValidate>
-                     <ButtonRow
-                        cancelButton={<Button variant="light" onClick={this.redirectCallback}><IoChevronBackCircleSharp size={30} color="#BFCADD" /></Button>}
-                        titlePage={<TitleRegister>{this.paramRoute === 'inserir' ? 'Cadastrar' : 'Editar'} Time</TitleRegister>}
-                     />
-                     <BackGroundForm xs={1} className={'mb-2'} noGutters>
-                        <Row>
-                           <Col className="mt-3" xs={12} sm={3}>
-                              <InputTextField
-                                 id="tea_name"
-                                 label="Nome"
-                                 type="text"
-                                 maxLength="45"
-                                 validated={this.state.validated}
-                                 defaultValue={this.state.primaryData?.tea_name}
-                                 errorMessage={this.state.tea_name}
-                                 onChange={this.handleChange}
-                                 required
-                              />
-                           </Col>
-                           <Col className="mt-3" xs={12} sm={3}>
-                              <ListCompanies
-                                 name="tea_cpn_cod"
-                                 validated={this.state.validated}
-                                 defaultCompany={this.state.primaryData.tea_cpn_cod ? this.state.primaryData.tea_cpn_cod : null}
-                                 errorMessage={this.state.tea_cpn_cod}
-                                 onChange={this.handleSelect}
-                              />
-                           </Col>
-                           <Col className="mt-3" xs={12} sm={3}>
-                              <BooleanField
-                                 Label="Status"
-                                 onFalse="Inativo"
-                                 onTrue="Ativo"
-                                 name="tea_active"
-                                 validated={this.state.validated}
-                                 value={this.state.primaryData?.tea_active}
-                                 onChange={this.handleSelect}
-                                 required />
-                           </Col>
-                           <Col className="mt-4" xs={10} sm={3}>
-                              <BtnBlue variant="dark" type="submit">Salvar</BtnBlue>
-                           </Col>
-                        </Row>
-                        <Row>
-                        </Row>
-                        {this.props.paramRoute === 'inserir'
-                           ? ''
-                           : (
-                              <Row className="mt-6">
-                                 <Col md={{ offset: 1 }} xs={12} sm={6}>
-                                    <DateField
-                                       controlId="tea_dtcreation"
-                                       Label="Data de criação:"
-                                       date={this.state.primaryData?.tea_dtcreation} />
-                                 </Col>
-                                 {this.state.primaryData?.tea_dtupdate === null
-                                    ? ''
-                                    : <Col xs={12} sm={6}>
-                                       <DateField
-                                          controlId="tea_dtupdate"
-                                          Label="Data de atualização:"
-                                          date={this.state.primaryData?.tea_dtupdate} />
-                                    </Col>
-                                 }
-                              </Row>
-                           )}
-                     </BackGroundForm>
-                  </Form>
-               )
-            }
-         </>
-      )
+export const TeamForm = (props) => {
+   const [primaryData, setPrimaryData] = useState()
+   const [fields, setFields] = useState(
+      {
+         tea_name: "",
+         tea_cpn_cod: "",
+         tea_active: ""
+      }
+   )
+   let method = "post"
+   let postEndpoint = "teams/cadastrar"
+   if (props.paramRoute !== "inserir") {
+      postEndpoint = "teams/alterar/" + props.primaryId
+      method = "put"
    }
+   useEffect(() => {
+      let tempFields = {}
+      const requestData = async () => {
+         try {
+            const data = await request({
+               method: "get",
+               endpoint: "teams/procurar/" + props.primaryId,
+            });
+            setPrimaryData(data.data)
+            for (const key of Object.keys(fields)) {
+               if (data.data[key] !== null) {
+                  tempFields[key] = data.data[key]
+               }
+               else {
+                  tempFields[key] = ""
+               }
+            }
+            setFields(tempFields)
+         } catch (error) {
+            console.log(error);
+         }
+      }
+      if (props.paramRoute !== "inserir") {
+         requestData();
+      }
+   }, [])
+   let validationSchema = yup.object({
+      tea_name: yup.string().max(45).required(),
+      tea_active: yup.bool().required()
+   })
+   return (
+      <>
+         <ButtonRow
+            cancelButton={<Button variant="light" onClick={props.redirectCallback}><IoChevronBackCircleSharp size={30} color="#BFCADD" /></Button>}
+            titlePage={<TitleRegister>{props.paramRoute === 'inserir' ? 'Cadastrar' : 'Editar'} Time</TitleRegister>}
+         />
+         <BackGroundForm xs={1} className={'mb-2'} noGutters>
+            <Formik
+               htmlFor="mainForm"
+               initialValues={fields}
+               validateOnBlur={false}
+               validationSchema={validationSchema}
+               onSubmit={
+                  async (values, { setSubmitting, setFieldError }) => {
+                     const data = await request({
+                        method: method,
+                        endpoint: postEndpoint,
+                        data: {
+                           ...values,
+                        },
+                     });
+                     if (data.meta.status === 100) {
+                        props.showAlert(data.meta)
+                     }
+                     else if (data.meta.status === 212) {
+                        props.showAlert(data.meta)
+                     }
+                     if (data.meta.status === 422) {
+                        setFieldError(data.data[0].field.toLowerCase(), data.data[0].message)
+                     }
+                     else {
+                        console.log("Uncaught exception")
+                     }
+                     setSubmitting(false);
+                  }
+               }
+               enableReinitialize
+            >{({ }) => (
+               <Form id="mainForm">
+                  <Row>
+                     <Col className="mt-3" xs={12} sm={3}>
+                        <DefaultValidationTextField
+                           name="tea_name"
+                           label="Nome"
+                           type="text"
+                           maxLength="45"
+                        />
+                     </Col>
+                     <Col className="mt-3" xs={12} sm={3}>
+                        <ListCompanies
+                           name="tea_cpn_cod"
+                           label="Empresa"
+                        />
+                     </Col>
+                     <Col className="mt-3" xs={12} sm={3}>
+                        <DefaultValidationTextField
+                           name="tea_active"
+                           label="Status"
+                           select>
+                           <MenuItem key={1} value={true}>Ativo</MenuItem>
+                           <MenuItem key={2} value={false}>Inativo</MenuItem>
+                        </DefaultValidationTextField>
+                     </Col>
+                     <Col className="mt-4" xs={12} sm={2}>
+                        <BtnBlue variant="dark" type="submit">Salvar</BtnBlue>
+                     </Col>
+                  </Row>
+                  {(props.paramRoute !== "inserir" && primaryData) ?
+                     <>
+                        <Timestamps
+                           primaryData={primaryData}
+                           fieldSuffix="tea_" />
+                     </>
+                     : null}
+               </Form>
+            )}
+            </Formik>
+         </BackGroundForm>
+      </>
+   )
 }
