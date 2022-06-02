97065a40d206059f454b451038c9004530dbb471
Alandysson Rolim <alandyssom@gmail.com>
2022-06-02 14:00:58 -0300

    Refactor(BadgeForm): Alterando as funções de controle de seletor para a nova versão

--- src/Pages/Badges/Cadastrar/Form.js
+++ src/Pages/Badges/Cadastrar/Form.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import { Form, Col, Row, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import yup from "../../../Services/validations";
+import { Form, Formik } from 'formik';
+import { Col, Row, Button } from 'react-bootstrap'
+import { DefaultValidationTextField } from '../../../Componentes/FormikComponents/DefaultValidationTextField';
+import { request } from '../../../Services/api';
 import { DateField } from '../../../Componentes/DateField'
 import { ButtonRow } from '../../../Componentes/ButtonRow'
 import { EditCreateForm } from '../../../Componentes/EditCreateForm/index'
@@ -8,15 +12,245 @@ import { CheckBox } from '../../../Componentes/CheckBox'
 import { CircularProgress } from '@material-ui/core'
 import { BackGroundForm, BtnBlue, TitleRegister } from '../../../styles/CommonStyles'
 import { IoChevronBackCircleSharp } from "react-icons/io5"
-import ListCompanies from '../../../Componentes/ListCompanies'
+import { ListCompanies } from '../../../Componentes/FormikComponents/ListCompanies';
 import ListJourneysControlled from "../../../Componentes/ListJourneysControlled"
+import { Timestamps } from '../../../Componentes/FormikComponents/Timestamps';
 
-export default function BadgeForm(props) {
-   return <BadgeFormBuilder insertDataEndpoint="badges/cadastrar"
-      requestDataEndpoint="badges/procurar/"
-      editDataEndpoint="badges/alterar/"
-      {...props} />
+export const BadgeForm = (props) => {
+   const [primaryData, setPrimaryData] = useState()
+   const [fields, setFields] = useState(
+      {
+         bdg_name: "",
+         bdg_cpn_cod: "",
+         bdg_mentor: false,
+         bdg_jny_cod: "",
+         bdg_description: "",
+         bdg_detail: "",
+      }
+   )
+   const [disableJourney, setDisableJourney] = useState(false);
+   const [disableCompany, setDisableCompany] = useState(false);
+   let method = "post"
+   let postEndpoint = "badges/cadastrar"
+   if (props.paramRoute !== "inserir") {
+      postEndpoint = "badges/alterar/" + props.primaryId
+      method = "put"
+   }
+   useEffect(() => {
+      let tempFields = {}
+      const requestData = async () => {
+         try {
+            const data = await request({
+               method: "get",
+               endpoint: "badges/procurar/" + props.primaryId,
+            });
+            setPrimaryData(data.data)
+            for (const key of Object.keys(fields)) {
+               if (data.data[key] !== null) {
+                  tempFields[key] = data.data[key]
+               }
+               else {
+                  tempFields[key] = ""
+               }
+            }
+            setFields(tempFields)
+         } catch (error) {
+            console.log(error);
+         }
+      }
+      if (props.paramRoute !== "inserir") {
+         requestData();
+      }
+   }, [])
+   let validationSchema = yup.object({
+      bdg_name: yup.string().max(45).required(),
+      bdg_description: yup.string().max(400).required(),
+      bdg_detail: yup.string().max(400).required(),
+   })
+   function handleChangeJourneyControlled(e) {
+      let cpn_value;
+      if (e.jny_cpn_cod !== undefined) {
+         companyCodeSetter(e.jny_cpn_cod)
+         setDisableCompany(true)
+         cpn_value = e.jny_cpn_cod
+      }
+      else {
+         setDisableCompany(false)
+         cpn_value = ""
+      }
+      journeyCodeSetter(e.value)
+      this.handleChange({ target: { id: "bdg_cpn_cod", value: String(cpn_value) } })
+   }
+
+   function journeyCodeSetter(journeyCode) {
+      // this.setState((state, props) => ({
+      //    primaryData: {
+      //       ...state.primaryData,
+      //       bdg_jny_cod: journeyCode
+      //    }
+      // }))
+   }
+   // TODO: create a empty menuitem to return to null value
+   function handleChangeCompanyControlled(e) {
+      {
+         if (e.target.value !== null && e.target.value !== "") {
+            setDisableJourney(true)
+         }
+         else {
+            setDisableJourney(false)
+         }
+      }
+      // companyCodeSetter(e.target.value)
+      // handleChange({ target: { id: "bdg_jny_cod", value: "" } })
+   }
+
+   function companyCodeSetter(companyCode) {
+      this.setState((state, props) => ({
+         primaryData: {
+            ...state.primaryData,
+            bdg_cpn_cod: companyCode
+         }
+      }))
+   }
+   return (
+      <>
+         <ButtonRow
+            cancelButton={<Button variant="light" onClick={props.redirectCallback}><IoChevronBackCircleSharp size={30} color="#BFCADD" /></Button>}
+            titlePage={<TitleRegister>{props.paramRoute === 'inserir' ? 'Cadastrar' : 'Editar'} Badge</TitleRegister>}
+         />
+         <BackGroundForm xs={1} className={'mb-2'} noGutters>
+            <Formik
+               htmlFor="mainForm"
+               initialValues={fields}
+               validateOnBlur={false}
+               validationSchema={validationSchema}
+               onSubmit={
+                  async (values, { setSubmitting, setFieldError }) => {
+                     const data = await request({
+                        method: method,
+                        endpoint: postEndpoint,
+                        data: {
+                           ...values,
+                        },
+                     });
+                     if (data.meta.status === 100) {
+                        props.showAlert(data.meta)
+                     }
+                     else if (data.meta.status === 212) {
+                        props.showAlert(data.meta)
+                     }
+                     if (data.meta.status === 422) {
+                        setFieldError(data.data[0].field.toLowerCase(), data.data[0].message)
+                     }
+                     else {
+                        console.log("Uncaught exception")
+                     }
+                     setSubmitting(false);
+                  }
+               }
+               enableReinitialize
+            >{({ setFieldValue, values }) => (
+               <Form id="mainForm">
+                  <Row>
+                     <Col className="mt-3" xs={12} sm={6}>
+                        <DefaultValidationTextField
+                           name="bdg_name"
+                           label="Nome"
+                           type="text"
+                           maxLength="45"
+                        />
+                     </Col>
+                     <Col className="mt-3" xs={12} sm={6}>
+                        <ListCompanies
+                           name="bdg_cpn_cod"
+                           label="Empresa"
+                           onChange={(event) => {
+                              handleChangeCompanyControlled(event)
+                              setFieldValue("bdg_cpn_cod", event.target.value)
+                           }}
+                        />
+                     </Col>
+                  </Row>
+                  <Row>
+                     <Col className="mt-3" xs={12} sm={6}>
+                        <CheckBox
+                           label="É badge de mentoria?"
+                           name="bdg_mentor"
+                           onChange={() => setFieldValue("bdg_mentor", !values.bdg_mentor)}
+                           checked={values.bdg_mentor}
+                        />
+                     </Col>
+                     <Col className="mt-3" xs={12} sm={6}>
+                        <ListJourneysControlled
+                           disabled={disableJourney}
+                           name="bdg_jny_cod"
+                           controlId="bdg_jny_cod"
+                           onChange={(event) => {
+                              let cpn_value;
+                              if (event.jny_cpn_cod !== undefined) {
+                                 setDisableCompany(true)
+                                 cpn_value = event.jny_cpn_cod
+                              }
+                              else {
+                                 setDisableCompany(false)
+                                 cpn_value = ""
+                              }
+                              // journeyCodeSetter(event.value)
+                              setFieldValue("bdg_cpn_cod", cpn_value)
+                           }}
+                        />
+                     </Col>
+                  </Row>
+                  <Row>
+                     <Col className="mt-4">
+                        <DefaultValidationTextField
+                           name="bdg_description"
+                           label="Descrição"
+                           type="textarea"
+                           maxLength="400"
+                           multiline
+                           rows={4}
+                        />
+                     </Col>
+                  </Row>
+                  <Row>
+                     <Col className="mt-4">
+                        <DefaultValidationTextField
+                           name="bdg_detail"
+                           label="Motivadores"
+                           type="textarea"
+                           maxLength="400"
+                           multiline
+                           rows={4}
+                        />
+                     </Col>
+                  </Row>
+                  {(props.paramRoute !== "inserir" && primaryData) ?
+                     <>
+                        <Timestamps
+                           primaryData={primaryData}
+                           fieldSuffix="bdg_" />
+                     </>
+                     : null}
+                  <Row>
+                     <Col md={{ offset: 5 }} className="mt-4">
+                        <BtnBlue variant="dark" type="submit">Salvar</BtnBlue>
+                     </Col>
+                  </Row>
+               </Form>
+            )}
+            </Formik>
+         </BackGroundForm>
+      </>
+   )
 }
+
+// export default function BadgeForm(props) {
+//    return <BadgeFormBuilder insertDataEndpoint="badges/cadastrar"
+//       requestDataEndpoint="badges/procurar/"
+//       editDataEndpoint="badges/alterar/"
+//       {...props} />
+// }
 
 export class BadgeFormBuilder extends EditCreateForm {
    constructor(props) {
